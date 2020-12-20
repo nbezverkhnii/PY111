282c078d80bc543ba5cba7590f6f23550966842e
nikolay24534 <nikolaybezverhny@gmail.com>
2020-12-20 18:00:51 +0300

    Проверка скобочек и использованием структуры данных стек. Как-то сложно яя это реализовал и мне не нравится. Но работает и тесты проходит.

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -5,5 +5,25 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
+    open_bracket = '('
+    close_bracket = ')'
+    my_stack = []
 
-    return False
+    # Случай "" исключительнй, если "", то ничего уже проверять не надо - сразу заканчиваем выполнение
+    if not brackets_row:
+        return True
+
+    for bracket in brackets_row:
+        if bracket == open_bracket:
+            my_stack.append(close_bracket)
+        # Если стек не пуст и в нем есть ), то удаляем из него последний элемент
+        elif my_stack and bracket == close_bracket:
+            my_stack.pop()
+        # Если стек пуст, а нам встретился ), то это False
+        elif not my_stack and bracket == close_bracket:
+            return False
+
+    return True if not my_stack else False
+
+if __name__ == '__main__':
+    print(check_brackets("(()))"))

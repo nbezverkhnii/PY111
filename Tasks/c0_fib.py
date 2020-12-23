99c8057c5795dfa88165fadbfa539034821a6ca7
nikolay24534 <nikolaybezverhny@gmail.com>
2020-12-23 19:33:38 +0300

    Рекурсивный и теративный фибоначи готов Все тесты пройдены

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -29,8 +29,10 @@ def fib_iterative(n: int) -> int:
     elif n < 0:
         raise ValueError()
 
-    b =
-    for i in range(n+1):
+    fib_list = [0, 1]
+    for i in range(2, n+1):
+        fib_list.append(fib_list[i-2] + fib_list[i-1])
 
+    return fib_list[-1]
 
 

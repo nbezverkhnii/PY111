a4377f5939aec7147093f50481943a2f783015a8
nikolay24534 <nikolaybezverhny@gmail.com>
2020-12-23 11:38:54 +0300

    Исправил задание a2_priority_queue в соответсвтии с замечаниями из ревью. Пофиксил возможный IndexError.

--- Tasks/a2_priority_queue.py
+++ Tasks/a2_priority_queue.py
@@ -8,7 +8,7 @@ from typing import Any
 # то может попробовать реалзовать очередь с приритетом
 # имеющую теоретическую сложность операций enqueue и dequeue О(1)
 # Однако, я не додумался как сделать dequeue О(1)
-my_prior_queue = [[] for _ in range(11)]
+my_priority_queue = [[] for _ in range(11)]
 
 def enqueue(elem: Any, priority: int = 0) -> None:
     """
@@ -16,7 +16,7 @@ def enqueue(elem: Any, priority: int = 0) -> None:
     :param elem: element to be added
     :return: Nothing
     """
-    my_prior_queue[priority].append(elem)
+    my_priority_queue[priority].append(elem)
     return None
 
 
@@ -26,29 +26,39 @@ def dequeue() -> Any:
     :return: dequeued element
     """
     result = None
-    for queue in my_prior_queue:
+    for priority in my_priority_queue:
         # Если не пуст
-        if queue:
-            result = queue.pop(0)
+        if priority:
+            result = priority.pop(0)
             break
     return result
 
 
 def peek(ind: int = 0, priority: int = 0) -> Any:
     """
-    Allow you to see at the element in the queue without dequeuing it
+    Allow you to see at the element by index and by priority in the queue without dequeuing it
     :param ind: index of element (count from the beginning)
+    :param priority: index of priority
     :return: peeked element
     """
-    return my_prior_queue[priority][ind] if my_prior_queue[priority][ind] else None
+    # Так как (преднаписаная) функция принимает два аргумента, то предполагается, что можно смотреть не только в
+    # очередь c наивысшим приоритетом, и не только на первый элемент. Иначе стоило бы вынести один из
+    # аргументов в константы или просто убрать его из аргументов.
+    # Такая реализация, не противоречит принципу работы Priority Queue, а просто немного раширяет функционал
+    try:
+        return my_priority_queue[priority][ind] if my_priority_queue[priority][ind] else None
+    except IndexError:
+        print(f'There is no element with index {ind} and priority {priority}')
+        return None
 
 
 def clear() -> None:
     """
     Clear my queue
+    It's clear the queue priority by priority
     :return: None
     """
-    for queue in my_prior_queue:
+    for queue in my_priority_queue:
         queue.clear()
     return None
 
@@ -56,7 +66,9 @@ if __name__ == '__main__':
     enqueue(3)
     enqueue(5)
     enqueue(7)
-    print(my_prior_queue)
+    print(my_priority_queue)
     print(peek())
     print(peek(1))
+    print(peek(3))
+    print(peek(1,1))
     print(peek())

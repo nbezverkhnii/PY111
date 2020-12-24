14cf9e2f9ad04500becfbbd19664946155c50f17
nikolay24534 <nikolaybezverhny@gmail.com>
2020-12-24 09:26:07 +0300

    Рекурсивный бинарный поиск - не доеделал до конца

--- Tasks/c2_recursive_binary_search.py
+++ Tasks/c2_recursive_binary_search.py
@@ -1,7 +1,7 @@
 from typing import Sequence, Optional
 
 
-def binary_search(elem: int, arr: Sequence) -> Optional[int]:
+def binary_search(elem: int, arr: Sequence, magic: int = 0) -> Optional[int]:
     """
     Performs binary search of given element inside of array (using recursive way)
 
@@ -9,5 +9,22 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    start = 0
+    end = len(arr) + 1
+
+    if len(arr) == 0:
+        return None
+    elif len(arr) == 1 and arr[0] != elem:
+        return None
+    elif len(arr) == 1 and arr[0] == elem:
+        return 0 + magic
+
+    while start <= end:
+        middle = int((start + end)/2)
+        if arr[middle] < elem:
+            magic += middle
+            binary_search(elem, arr[middle+1:end], magic)
+        elif arr[middle] > elem:
+            magic -= middle
+            binary_search(elem, arr[start:middle-1], magic)
+

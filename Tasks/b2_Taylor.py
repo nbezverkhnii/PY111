5d4d27057af713659f5f45f4d91e15051da1538e
nikolay24534 <nikolaybezverhny@gmail.com>
2020-12-24 13:45:06 +0300

    Посчитал приближенное значение экспонент и синуса разложением в ряд Все тесты пройдены

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,7 +2,10 @@
 Taylor series
 """
 from typing import Union
+from math import factorial
+from itertools import count
 
+DELTA = 0.0001
 
 def ex(x: Union[int, float]) -> float:
     """
@@ -11,8 +14,15 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    ex_sum = 0
+    for n in count():
+        a = pow(x, n)
+        b = factorial(n)
+        ex_sum += a / b
+
+        if abs(a / b) <= DELTA:
+            return ex_sum
+
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -22,5 +32,12 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    sin_sum = 0
+    for n in count(1):
+        a = pow(x, 2 * n - 1)
+        b = factorial(2 * n - 1)
+        res = pow(-1, n + 1) * a / b
+        sin_sum += res
+
+        if abs(res) <= DELTA:
+            return sin_sum

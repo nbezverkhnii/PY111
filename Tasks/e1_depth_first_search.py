f7af219e9f26000b33090d31ca798854d8c510fe
nikolay24534 <nikolaybezverhny@gmail.com>
2021-01-08 15:50:49 +0300

    Поиск в глубину. Не рекурсивный.

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -10,5 +10,29 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    was_visited: List[Hashable] = list()
+    stack: List[Hashable] = list()
+    stack.append(start_node)
+
+    while stack:
+        current_value = stack.pop()
+        for i in g.neighbors(current_value):
+            if i not in was_visited and i not in stack:
+                stack.append(i)
+        was_visited.append(current_value)
+
+    return was_visited
+
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFG")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'C'),
+        ('B', 'D'),
+        ('B', 'E'),
+        ('C', 'F'),
+        ('E', 'G'),
+    ])
+    print(dfs(graph, 'A'))

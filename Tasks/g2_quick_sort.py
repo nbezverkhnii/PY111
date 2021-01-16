b8952b59a966752e945ccc1305235d4490f72b46
nikolay24534 <nikolaybezverhny@gmail.com>
2021-01-16 15:32:32 +0300

    Быстрая сортировка. Из книги Грокаем Алгоритмы.

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -1,4 +1,5 @@
 from typing import List
+import random
 
 
 def sort(container: List[int]) -> List[int]:
@@ -8,4 +9,17 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+    if len(container) <= 1:
+        return container
+
+    pivot = container[0]
+    less = [i for i in container[1:] if i <= pivot]
+    greather = [i for i in container[1:] if i >= pivot]
+
+    return sort(less) + [pivot] + sort(greather)
+
+
+if __name__ == '__main__':
+    arr = [52, 12, 3, 89, 43, 28, 66, 1, 28, 7, 85, 74, 19, 35, 2]
+    print(arr)
+    print(sort(arr))
